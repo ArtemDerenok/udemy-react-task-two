a825d1793b72c6af74ad52624286b4e96ac35e1a
Artem <derenokartem95@gmail.com>
2022-05-30 15:13:54 +0300

    feat: add spinner and error handler

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,8 +1,10 @@
 import { Component } from 'react';
-import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from './../../services/MarvelService';
 import getRandomNum from './../../utils/getRandomNum';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from './../errorMessage/errorMessage';
+import './randomChar.scss';
 
 class RandomChar extends Component {
     
@@ -10,6 +12,8 @@ class RandomChar extends Component {
         super(props);
         this.state = {
             char: {},
+            loading: true,
+            error: false,
         }
     }
     
@@ -18,7 +22,7 @@ class RandomChar extends Component {
     }
     
     onCharLoaded = (char) => {
-        this.setState({char});
+        this.setState({char, loading: false});
         this.onCutDescription();
     }
     
@@ -28,7 +32,17 @@ class RandomChar extends Component {
         const id = getRandomNum(1011000, 1011400);
         
         this.marvelService.getCharacter(id)
-        .then(res => this.onCharLoaded(res));
+        .then(res => this.onCharLoaded(res))
+        .catch(e => {
+            this.onError();
+        }) ;
+    }
+    
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true,
+        })
     }
     
     onCutDescription = () => {
@@ -45,27 +59,16 @@ class RandomChar extends Component {
     }
     
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char} /> : null; 
         
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {!description ? 'Description is not found' : description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -82,6 +85,28 @@ class RandomChar extends Component {
             </div>
         )
     }
+}
+
+const View = ({char: {name, description, thumbnail, homepage, wiki}}) => {
+    return (
+        <div className="randomchar__block">
+        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+        <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">
+                {!description ? 'Description is not found' : description}
+            </p>
+            <div className="randomchar__btns">
+                <a href={homepage} className="button button__main">
+                    <div className="inner">homepage</div>
+                </a>
+                <a href={wiki} className="button button__secondary">
+                    <div className="inner">Wiki</div>
+                </a>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default RandomChar;
