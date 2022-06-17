625a9914d3702257de8e27690080c7eaa5f66f59
Artem <derenokartem95@gmail.com>
2022-06-17 21:37:31 +0300

    feat: add css transition router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useState, lazy, Suspense} from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import decoration from '../../resources/img/vision.png';
-import {MainPage, ComicsPage, NotFound, SingleComic} from '../pages';
+import {ComicsPage, SingleComic} from '../pages';
+import Spinner from "../spinner/Spinner";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import './app.scss';
+const NotFound = lazy(() => import('../pages/NotFound'))
+const MainPage = lazy(() => import('../pages/MainPage'))
 
 const App = () => {
     const [selectedChar, setSelectedChar] = useState(null)
@@ -10,18 +15,26 @@ const App = () => {
     const onSelectedChar = (id) => {
         setSelectedChar(id)
     }
+    
+    const location = useLocation();
 
     return (
             <div className="app">
             <AppHeader/>
             <main>
-            <Routes>
-                <Route path="/" element={<MainPage onSelectedChar={onSelectedChar} selectedChar={selectedChar} />} />
-                <Route path="/comics" element={<ComicsPage />} />
-                <Route path="/comics/:comicId" element={<SingleComic />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+            <Suspense fallback={<Spinner />}>
+                <TransitionGroup>
+                <CSSTransition timeout={300} classNames='page' key={location.key}>
+                    <Routes location={location}>
+                        <Route path="/" element={<MainPage onSelectedChar={onSelectedChar} selectedChar={selectedChar} />} />
+                        <Route path="/comics" element={<ComicsPage />} />
+                        <Route path="/comics/:comicId" element={<SingleComic />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </CSSTransition>
+                </TransitionGroup>
+            </Suspense>    
+            <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
             </div>
     )
